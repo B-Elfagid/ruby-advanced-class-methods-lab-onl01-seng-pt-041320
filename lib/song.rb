7224f8c263d98cfbea91e122e017391f9525d5ec
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-07 20:02:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-  self.find_by_name(name) || self.create_by_name(name)
+  if self.find_by_name(name) || self.create_by_name(name)
   end 
   
   def self.alphabetical
@@ -45,7 +45,7 @@ class Song
     song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    song.save
+    song save
     song
   end
   
